f43f1a358154f2f4c518ce2c458c896ceb0544d2
mdodge2 <mdodge@hawaii.edu>
2022-08-28 21:35:46 -1000

    add skeleton of task.sh for ingestor

---
+++ containers/ingestion/scripts/task.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "[task.sh] [1/5] Starting Execution."
+
+echo "[task.sh] [2/5] Injecting authentication into config."
+
+echo "[task.sh] [3/5] Injecting configuration data into config."
+
+echo "[task.sh] [4/5] Injecting file list into config."
+
+echo "[task.sh] [5/5] Ingesting station values."
