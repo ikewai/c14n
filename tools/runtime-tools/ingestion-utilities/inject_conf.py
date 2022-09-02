b537c5b02d326891d51c608a81e86eb5ece326d6
mdodge2 <mdodge@hawaii.edu>
2022-09-01 16:07:56 -1000

    implement inject_conf for ingestion (fix)

--- tools/runtime-tools/ingestion-utilities/inject_conf.py
+++ tools/runtime-tools/ingestion-utilities/inject_conf.py
@@ -6,3 +6,27 @@ import os # For accessing environment variables.
 import sys # For accessing execution args.
 from io import TextIOWrapper # For hinting type in file read/writes.
 import json # For interacting with JSON data.
+
+# Get configuration JSON from environment variable. 
+config_env_key: str = str(sys.argv[1])
+config_env: str = str(os.environ[config_env_key])
+config_env: dict = json.loads(config_env)
+
+# Get configuration dict from json file.
+fin: TextIOWrapper = open(sys.argv[2], "rt")
+fin_string: str = str(fin.read())
+fin.close()
+config_json: dict = json.loads(fin_string)
+
+# Set data values from environment variable.
+config_json['data'] = config_env['data']
+
+# Convert output dict to JSON-formatted string.
+config_str: str = json.dumps(config_json)
+
+# Write updated config to disk.
+fout: TextIOWrapper = open(sys.argv[2], "wt")
+fout.write(config_str)
+fout.close()
+
+# Done.
