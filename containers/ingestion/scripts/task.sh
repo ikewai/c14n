2426a970fc7af965935b3beb6ed49056bd8a158f
mdodge2 <mdodge@hawaii.edu>
2022-09-05 09:39:37 -1000

    add env keys to task.sh syntaxes

--- containers/ingestion/scripts/task.sh
+++ containers/ingestion/scripts/task.sh
@@ -2,10 +2,10 @@
 echo "[task.sh] [1/4] Starting Execution."
 
 echo "[task.sh] [2/4] Injecting authentication into config."
-python3 /actor/inject_auth.py /home/hcdp_tapis_injector/station_values/config.json
+python3 /actor/inject_auth.py IW_TOKEN /home/hcdp_tapis_injector/station_values/config.json
 
 echo "[task.sh] [3/4] Injecting configuration data into config."
-python3 /actor/inject_conf.py /home/hcdp_tapis_injector/station_values/config.json
+python3 /actor/inject_conf.py DRIVER_CONF /home/hcdp_tapis_injector/station_values/config.json
 
 echo "[task.sh] [4/4] Ingesting station values."
 python3 /home/hcdp_tapis_injector/station_values/driver.py
