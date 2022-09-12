3bb1f91e958441494b8349454fdefe5c0cbb3264
mdodge2 <mdodge@hawaii.edu>
2022-09-11 21:09:16 -1000

    clean up rf monthly task.sh; clear todo from doc

--- containers/rainfall/preliminary/aggregation/README.md
+++ containers/rainfall/preliminary/aggregation/README.md
@@ -4,14 +4,3 @@ Contents:
 * [`base`](/containers/rainfall/preliminary/base): Essential libraries, scripts, and directory setup.
 * [`daily`](/containers/rainfall/preliminary/daily): Daily aggregation.
 * [`monthly`](/containers/rainfall/preliminary/monthly): Monthly aggregation **and** mapping.
-
-----
-TODO: monthly agg needs additional inputs:
-* ikewai-annotated-data/HCDP/production/rainfall/new/day/statewide/partial/station_data/2022/07
-* rainfall_new_day_statewide_partial_station_data_2022_07.csv
-
-where the month/year labels are adjusted relevantly.
-
-when downloaded, output it on the local container side as:
-`Statewide_Partial_Filled_Daily_RF_mm_2022_07.csv`
-within `/home/hawaii_climate_products_container/preliminary/rainfall/(more info here)`
--- containers/rainfall/preliminary/aggregation/monthly/scripts/task.sh
+++ containers/rainfall/preliminary/aggregation/monthly/scripts/task.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 CONDA_CMD="/root/anaconda3/bin/conda run --no-capture-output -n container_env"
-echo "[task.sh] Starting Execution."
+echo "[task.sh] [1/4] Starting Execution."
 
-echo "[task.sh] Acquiring Daily Dependencies."
+echo "[task.sh] [2/6] Acquiring and decompressing Daily Dependencies Archive."
 cd /home/hawaii_climate_products_container/preliminary/rainfall/dependencies
 wget https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/rainfall/HCDP_dependicies/daily_dependencies.tar.gz
 tar -xf daily_dependencies.tar.gz
 rm daily_dependencies.tar.gz
-wget https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/production/rainfall/new/day/statewide/partial/station_data/`date +%Y`/`date +%m`
 
-echo "[task.sh] Aggregating Rainfall data on the monthly timeframe."
+echo "[task.sh] [3/5] Aggregating Rainfall data on the monthly timeframe."
 cd /home/hawaii_climate_products_container/preliminary/rainfall/code/monthly
 $CONDA_CMD Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/monthly/daily_to_monthly_agg_FINAL.R
 $CONDA_CMD Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/monthly/monthly_rf_krig_map_makr_FINAL.R
 
-
-echo "[task.sh] [disabled] Preparing for upload."
+echo "[task.sh] [4/5] Preparing for upload."
 cd /sync
-#python3 update_date_string_in_config.py upload_config.json upload_config_datestrings_loaded.json
-#python3 upload_list_inserter.py upload_config_datestrings_loaded.json config.json
-#python3 upload_auth_injector.py config.json
+python3 update_date_string_in_config.py upload_config.json upload_config_datestrings_loaded.json
+python3 upload_list_inserter.py upload_config_datestrings_loaded.json config.json
+python3 upload_auth_injector.py config.json
 
-echo "[task.sh] [disabled] Attempting to upload the aggregated data."
+echo "[task.sh] [5/5] [disabled] Attempting to upload the aggregated data."
 #python3 upload.py
 
 echo "[task.sh] All done!"
