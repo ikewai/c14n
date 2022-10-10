a1798dc8e7029f365dd9fbfb7b5d18b9a15367a7
mdodge2 <mdodge@hawaii.edu>
2022-10-10 07:53:23 -1000

    potential fix for upload list adder

--- tools/runtime-tools/uploader-utilities/add_upload_list_to_config.py
+++ tools/runtime-tools/uploader-utilities/add_upload_list_to_config.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 import json, sys
 
 # load in the basic upload data
@@ -19,16 +20,22 @@ dir_permissions = [
 for upload_dir in upload_config['upload']:
     upload_dir['dir_permissions'] = dir_permissions
 
+fout = open(sys.argv[2], "wt")
+json.dump(upload_config, fout, indent=4)
+fout.close()
+
+
+### the following seems weirdly organized, pending deletion
 # load config file to place the new upload list into
-fin = open(sys.argv[2], "rt")
-fin_string = str(fin.read())
-fin.close()
+#fin = open(sys.argv[2], "rt")
+#fin_string = str(fin.read())
+#fin.close()
 
-full_upload_config = json.loads(fin_string)
+#full_upload_config = json.loads(fin_string)
 
-full_upload_config['upload'] = upload_config['upload']
+#full_upload_config['upload'] = upload_config['upload']
 
 # write the new config
-fout = open(sys.argv[2], "wt")
-json.dump(full_upload_config, fout, indent=4)
-fout.close()
+#fout = open(sys.argv[2], "wt")
+#json.dump(full_upload_config, fout, indent=4)
+#fout.close()
