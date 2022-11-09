b874575a8a9be2e20eb36735f3ed89e455c3b8c4
mdodge2 <mdodge@hawaii.edu>
2022-11-09 10:35:02 -1000

    add no-cache option to batch container builder

--- tools/dev-tools/build_containers_from_list.py
+++ tools/dev-tools/build_containers_from_list.py
@@ -4,6 +4,7 @@
 import sys # For accessing execution args.
 import subprocess # For spinning off docker builds.
 import json # For parsing the build list.
+import os # For determining no-cache
 
 fin = sys.argv[1]
 fin = open(fin, "rt")
@@ -12,5 +13,6 @@ container_list = json.loads(fin.read())
 for c in container_list:
     cmd = f"docker build \
             -f {c['rel_location']}/Dockerfile \
+            {'--no-cache' if os.environ['BUILD_NO_CACHE']==1 else ''} \
             -t {c['image_name']}:{c['tag']} ."
     subprocess.run(["/bin/bash", "-c", cmd])
