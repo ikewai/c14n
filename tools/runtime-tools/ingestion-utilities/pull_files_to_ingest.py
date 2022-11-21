52c7d05619be985f2f3b28452540d5481ce07e66
mdodge2 <mdodge@hawaii.edu>
2022-11-21 08:44:01 -1000

    fix indexing and presence of get_auth_token.py

--- tools/runtime-tools/ingestion-utilities/pull_files_to_ingest.py
+++ tools/runtime-tools/ingestion-utilities/pull_files_to_ingest.py
@@ -9,7 +9,7 @@ fname = str(sys.argv[1])
 with open(fname, "rt") as config_file:
     config_dict: dict = json.loads(config_file.read())
 
-file_pulls: list(str) = config_dict["data"]["file_pulls"]
+file_pulls: list(str) = config_dict["data"][0]["file_pulls"]
 
 for pull in file_pulls:
     mkdir_command: str = f"mkdir -p {pull['placement_dir']}"
