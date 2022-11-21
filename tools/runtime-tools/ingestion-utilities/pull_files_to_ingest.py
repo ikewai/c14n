5c1e03ccdd9c7a3cd54e9a9e1c8c7888f5986984
mdodge2 <mdodge@hawaii.edu>
2022-11-21 08:24:22 -1000

    implement ingestion file puller; update container

---
+++ tools/runtime-tools/ingestion-utilities/pull_files_to_ingest.py
@@ -0,0 +1,18 @@
+# Downloads files according to the "file_pulls" field in an ingestion config.
+
+import json
+import sys
+import subprocess
+
+fname = str(sys.argv[1])
+
+with open(fname, "rt") as config_file:
+    config_dict: dict = json.loads(config_file.read())
+
+file_pulls: list(str) = config_dict["data"]["file_pulls"]
+
+for pull in file_pulls:
+    mkdir_command: str = f"mkdir -p {pull['placement_dir']}"
+    subprocess.run("/bin/bash", "-c", mkdir_command)
+    wget_command: str = f"wget {pull['url']} -O {pull['placement_dir']}/{pull['rename']}"
+    subprocess.run(["/bin/bash", "-c", wget_command])
