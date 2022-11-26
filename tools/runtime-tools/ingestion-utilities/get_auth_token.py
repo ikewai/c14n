2c4c68e9b68b6ffdd9d94f7cdb0c149555063f51
mdodge2 <mdodge@hawaii.edu>
2022-11-26 13:02:16 -1000

    update get_auth_token_py for ingestor with tempfix

--- tools/runtime-tools/ingestion-utilities/get_auth_token.py
+++ tools/runtime-tools/ingestion-utilities/get_auth_token.py
@@ -6,18 +6,24 @@ import sys # For getting execution arguments.
 from agavepy import Agave # For communicating with the gateway.
 import subprocess # For setting the token's env variable via bash export.
 
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+os.environ['CURL_CA_BUNDLE']="" # This is a temporary hack. It will be fixed soon.
+
 token_env_key = sys.argv[1]
 
 # Initialize Agave object.
 ag = Agave(api_server="https://agaveauth.its.hawaii.edu",
            username=os.environ['IW_USERNAME'],
            password=os.environ['IW_PASSWORD'],
+           client_name=os.environ['IW_CLIENT_NAME'],
            api_key=os.environ['IW_API_KEY'],
            api_secret=os.environ['IW_API_SECRET']
 )
 
 # Acquire tokens.
-ag.refresh()
+
 new_tokens = ag.tokens()
 access_token = new_tokens['access_token']
 
