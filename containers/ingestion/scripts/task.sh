dba0a9182c96eb06f20b2353447082b9bedaf9ce
mdodge2 <mdodge@hawaii.edu>
2022-11-26 14:56:30 -1000

    remove unnecessary inject_conf

--- containers/ingestion/scripts/task.sh
+++ containers/ingestion/scripts/task.sh
@@ -2,10 +2,10 @@
 echo "[task.sh] Starting Execution."
 cd /home/hcdp_tapis_ingestor/station_values
 
-echo "[task.sh] [1/7] Downloading ingestion config."
+echo "[task.sh] [1/6] Downloading ingestion config."
 wget $INGESTION_CONFIG_URL -O config.json
 
-echo "[task.sh] [2/7] Updating date strings in config if requested."
+echo "[task.sh] [2/6] Updating date strings in config if requested."
 if [ $UPDATE_DATES_IN_CONFIG ];
 then
     mv config.json config_temp.json
@@ -14,19 +14,16 @@ then
     config.json
 fi
 
-echo "[task.sh] [3/7] Pulling files to ingest."
+echo "[task.sh] [3/6] Pulling files to ingest."
 python3 /actor/pull_files_to_ingest.py /home/hcdp_tapis_ingestor/station_values/config.json
 
-echo "[task.sh] [4/7] Getting and setting authentication token."
+echo "[task.sh] [4/6] Getting and setting authentication token."
 python3 /actor/get_auth_token.py IW_TOKEN.txt
 
-echo "[task.sh] [5/7] Injecting authentication into config."
+echo "[task.sh] [5/6] Injecting authentication into config."
 python3 /actor/inject_auth.py IW_TOKEN.txt /home/hcdp_tapis_ingestor/station_values/config.json
 
-echo "[task.sh] [6/7] Injecting configuration data into config."
-python3 /actor/inject_conf.py DRIVER_CONF /home/hcdp_tapis_ingestor/station_values/config.json
-
-echo "[task.sh] [7/7] Ingesting station values."
+echo "[task.sh] [6/6] Ingesting station values."
 python3 /home/hcdp_tapis_injector/station_values/driver.py
 
 echo "[task.sh] All done!"
