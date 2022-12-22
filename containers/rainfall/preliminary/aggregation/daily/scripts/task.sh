0ece7f43f466ce86871cc28238dc60d84bb7b8e3
mdodge2 <mdodge@hawaii.edu>
2022-12-22 13:13:16 -1000

    fix missing r-base; remove conda from tasks

--- containers/rainfall/preliminary/aggregation/daily/scripts/task.sh
+++ containers/rainfall/preliminary/aggregation/daily/scripts/task.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-CONDA_CMD="/root/anaconda3/bin/conda run --no-capture-output -n container_env"
 echo "[task.sh] [1/5] Starting Execution."
 
 echo "[task.sh] [2/5] Acquiring Daily Dependencies."
@@ -10,12 +9,12 @@ rm daily_dependencies.tar.gz
 
 echo "[task.sh] [3/5] Aggregating Rainfall data on the daily timeframe."
 cd /home/hawaii_climate_products_container/preliminary/rainfall/code/daily
-$CONDA_CMD Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/rcode/hads_daily_rf_FINAL.R
-$CONDA_CMD Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/rcode/nws_rr5_daily_rf_FINAL.R
-$CONDA_CMD Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/rcode/madis_daily_rf_FINAL.R
-$CONDA_CMD Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/rcode/all_data_daily_merge_table_rf_FINAL.R
-$CONDA_CMD Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/rcode/qaqc_randfor_bad_data_flag_remove_rf_FINAL.R
-$CONDA_CMD Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/rcode/daily_gap_fill_NR_only_rf_FINAL.R
+Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/rcode/hads_daily_rf_FINAL.R
+Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/rcode/nws_rr5_daily_rf_FINAL.R
+Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/rcode/madis_daily_rf_FINAL.R
+Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/rcode/all_data_daily_merge_table_rf_FINAL.R
+Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/rcode/qaqc_randfor_bad_data_flag_remove_rf_FINAL.R
+Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/rcode/daily_gap_fill_NR_only_rf_FINAL.R
 
 echo "[task.sh] [4/5] Preparing for upload."
 cd /sync
