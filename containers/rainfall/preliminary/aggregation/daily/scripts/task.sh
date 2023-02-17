e32f34d8499cde134bf1294eb58f8a715e0d8c86
mdodge2 <mdodge@hawaii.edu>
2023-02-17 13:27:39 -1000

    add new daily wget to rainfall

--- containers/rainfall/preliminary/aggregation/daily/scripts/task.sh
+++ containers/rainfall/preliminary/aggregation/daily/scripts/task.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-echo "[task.sh] [1/5] Starting Execution."
+echo "[task.sh] [1/6] Starting Execution."
 
-echo "[task.sh] [2/5] Acquiring Daily Dependencies."
+echo "[task.sh] [2/6] Acquiring Daily Dependencies."
 cd /home/hawaii_climate_products_container/preliminary/rainfall/dependencies
 wget https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/rainfall/HCDP_dependicies/daily_dependencies.tar.gz
 tar -xf daily_dependencies.tar.gz
 rm daily_dependencies.tar.gz
 
-CURRENT_YEAR=$(date +"%Y")
-CURRENT_MONTH=$(date +"%m")
-wget https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/production/rainfall/new/day/statewide/partial/station_data/$CURRENT_YEAR/$CURRENT_MONTH/rainfall_new_day_statewide_partial_station_data_$CURRENT_YEAR_$CURRENT_MONTH.csv
-wget https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/production/rainfall/new/day/statewide/raw/station_data/$CURRENT_YEAR/$CURRENT_MONTH/rainfall_new_day_statewide_raw_station_data_$CURRENT_YEAR_$CURRENT_MONTH.csv
+echo "[task.sh] [3/6] Acquiring yesterday's cumulative aggregation file, if it exists."
+cd /home/hawaii_climate_products_container
+echo "---rf_daily_wget.sh---"
+bash rf_daily_wget.sh
 
-echo "[task.sh] [3/5] Aggregating Rainfall data on the daily timeframe."
+echo "[task.sh] [4/6] Aggregating Rainfall data on the daily timeframe."
 cd /home/hawaii_climate_products_container/preliminary/rainfall/code/daily
 echo "---hads_daily_rf_FINAL.R---"
 Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/rcode/hads_daily_rf_FINAL.R
@@ -27,13 +27,13 @@ Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/
 echo "---daily_gap_fill_NR_only_rf_FINAL.R---"
 Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/rcode/daily_gap_fill_NR_only_rf_FINAL.R
 
-echo "[task.sh] [4/5] Preparing for upload."
+echo "[task.sh] [5/6] Preparing for upload."
 cd /sync
 python3 update_date_string_in_config.py upload_config.json upload_config_datestrings_loaded.json
 python3 add_upload_list_to_config.py upload_config_datestrings_loaded.json config.json
 python3 add_auth_info_to_config.py config.json
 
-echo "[task.sh] [5/5] Attempting to upload the aggregated data."
+echo "[task.sh] [6/6] Attempting to upload the aggregated data."
 python3 upload.py
 
 echo "[task.sh] All done!"
