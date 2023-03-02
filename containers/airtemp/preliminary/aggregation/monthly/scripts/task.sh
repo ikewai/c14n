2804acf895344eaaa2f16ad4f2a84d73c384d48c
mdodge2 <mdodge@hawaii.edu>
2023-03-02 09:54:52 -1000

    add use of aggregation date to date string updater

--- containers/airtemp/preliminary/aggregation/monthly/scripts/task.sh
+++ containers/airtemp/preliminary/aggregation/monthly/scripts/task.sh
@@ -26,7 +26,7 @@ echo "---monthly_stn_data.py--- on Tmin"
 python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/monthly/code/monthly_stn_data.py Tmin $AGGREGATION_DATE
 echo "[task.sh] [3/4] Preparing to upload data."
 cd /sync
-python3 update_date_string_in_config.py upload_config.json upload_config_datestrings_loaded.json
+python3 update_date_string_in_config.py upload_config.json upload_config_datestrings_loaded.json $AGGREGATION_DATE
 python3 add_upload_list_to_config.py upload_config_datestrings_loaded.json config.json $AGGREGATION_DATE
 python3 add_auth_info_to_config.py config.json
 
