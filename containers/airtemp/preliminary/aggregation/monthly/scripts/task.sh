472554c063c9b17339f0d0d1bebeac008679d883
mdodge2 <mdodge@hawaii.edu>
2023-03-02 09:38:38 -1000

    fix airtemp monthly task

--- containers/airtemp/preliminary/aggregation/monthly/scripts/task.sh
+++ containers/airtemp/preliminary/aggregation/monthly/scripts/task.sh
@@ -20,13 +20,14 @@ echo "---monthly_meta_wrapper.py---"
 python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/monthly/code/monthly_meta_wrapper.py $AGGREGATION_DATE
 echo "---monthly_state_wrapper.py---"
 python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/monthly/code/monthly_state_wrapper.py $AGGREGATION_DATE
-echo "---monthly_stn_data.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/monthly/code/monthly_stn_data.py $AGGREGATION_DATE
-
+echo "---monthly_stn_data.py--- on Tmax"
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/monthly/code/monthly_stn_data.py Tmax $AGGREGATION_DATE
+echo "---monthly_stn_data.py--- on Tmin"
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/monthly/code/monthly_stn_data.py Tmin $AGGREGATION_DATE
 echo "[task.sh] [3/4] Preparing to upload data."
 cd /sync
 python3 update_date_string_in_config.py upload_config.json upload_config_datestrings_loaded.json
-python3 add_upload_list_to_config.py upload_config_datestrings_loaded.json config.json $CUSTOM_TIME
+python3 add_upload_list_to_config.py upload_config_datestrings_loaded.json config.json $AGGREGATION_DATE
 python3 add_auth_info_to_config.py config.json
 
 echo "[task.sh] [4/4] Uploading data."
