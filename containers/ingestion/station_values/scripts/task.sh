65cfe6960f3824f7b8a5b9ebd7f055eb17481101
mdodge2 <mdodge@hawaii.edu>
2023-04-05 10:55:23 -1000

    add custom date feature to ingestion

--- containers/ingestion/station_values/scripts/task.sh
+++ containers/ingestion/station_values/scripts/task.sh
@@ -8,10 +8,19 @@ wget $INGESTION_CONFIG_URL -O config.json
 echo "[task.sh] [2/6] Updating date strings in config if requested."
 if [ $UPDATE_DATES_IN_CONFIG ];
 then
-    mv config.json config_temp.json
-    python3 /actor/update_date_string_in_config.py \
-    config_temp.json \
-    config.json
+    if [ $CUSTOM_DATE ];
+    then
+        mv config.json config_temp.json
+        python3 /actor/update_date_string_in_config.py \
+        config_temp.json \
+        config.json \
+        $CUSTOM_DATE
+    else
+        mv config.json config_temp.json
+        python3 /actor/update_date_string_in_config.py \
+        config_temp.json \
+        config.json
+    fi
 fi
 
 echo "[task.sh] [3/6] Pulling files to ingest."
