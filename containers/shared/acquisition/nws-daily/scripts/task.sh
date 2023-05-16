881febb036f1d85fa4945b11c0bee0ccdff6318c
mdodge2 <mdodge@hawaii.edu>
2023-05-16 08:03:14 -1000

    clarify comment in nws aq task.sh

--- containers/shared/acquisition/nws-daily/scripts/task.sh
+++ containers/shared/acquisition/nws-daily/scripts/task.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "[task.sh] [1/4] Starting Execution."
 
-echo "[task.sh] [2/4] Collecting Climate data from MADIS on the daily timeframe."
+echo "[task.sh] [2/4] Collecting Climate data from NWS on the daily timeframe."
 cd /home/hawaii_climate_products_container/preliminary/data_aqs/code/nws_rr5
 Rscript nws_rr5_hrly_24hr_webscape.R
 
