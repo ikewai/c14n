d7d67035458b78d71f8ecc2c425e0e8331c1c6cf
Michael Dodge <mdodge@hawaii.edu>
2023-06-20 14:52:04 -1000

    improve script documentation

--- tools/runtime-tools/ingestion-utilities/pull_files_to_ingest.py
+++ tools/runtime-tools/ingestion-utilities/pull_files_to_ingest.py
@@ -1,17 +1,20 @@
-# Pulls the files specified by the download section of an ingestion config
+#!/usr/bin/env python3
+# Pulls the files specified by the download section of an ingestion config.
 
 import json
 import sys
 import subprocess
 
+# Load the config file specified as the first command line argument
 fname = str(sys.argv[1])
-
 with open(fname, "rt") as config_file:
     config_dict: dict = json.loads(config_file.read())
 
+# Extract the base URL and list of files to download from the config
 base_url = config_dict["download_base_url"]
 files_to_download = config_dict["download"]
 
+# Download each file using wget
 for file in files_to_download:
     wget_cmd: str = f"wget -P /ingest {base_url}{file}"
     subprocess.run(["/bin/bash", "-c", wget_cmd])
--- tools/runtime-tools/uploader-utilities/add_upload_list_to_config.py
+++ tools/runtime-tools/uploader-utilities/add_upload_list_to_config.py
@@ -1,12 +1,12 @@
 import json, sys
 
-# load in the basic upload data
+# Load the config file specified as the first command line argument
 fin = open(sys.argv[1], "rt")
 fin_string = str(fin.read())
 fin.close()
-
 upload_config = json.loads(fin_string)
 
+# Default permission setup for uploading data for public access.
 dir_permissions = [
     {
         "permission": "READ",
@@ -19,22 +19,7 @@ dir_permissions = [
 for upload_dir in upload_config['upload']:
     upload_dir['dir_permissions'] = dir_permissions
 
+# write updated config to disk
 fout = open(sys.argv[2], "wt")
 json.dump(upload_config, fout, indent=4)
 fout.close()
-
-
-### the following seems weirdly organized, pending deletion
-# load config file to place the new upload list into
-#fin = open(sys.argv[2], "rt")
-#fin_string = str(fin.read())
-#fin.close()
-
-#full_upload_config = json.loads(fin_string)
-
-#full_upload_config['upload'] = upload_config['upload']
-
-# write the new config
-#fout = open(sys.argv[2], "wt")
-#json.dump(full_upload_config, fout, indent=4)
-#fout.close()
