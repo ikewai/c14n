604d88db37bdc5d07d7e5cf7b467b5e1016c2b86
Michael Dodge <mdodge@hawaii.edu>
2023-09-01 10:47:43 -1000

    add possible date range feature

--- tools/dev-tools/batch_run_containers.py
+++ tools/dev-tools/batch_run_containers.py
@@ -10,6 +10,10 @@
 #     "2023-01-01",
 #     "2023-01-02",
 #     "2023-01-03"
+#   ],
+#   "date_ranges": [
+#     "2023-01-01_2023-01-05",
+#     "2023-01-06_2023-01-10"
 #   ]
 # }
 
@@ -18,7 +22,20 @@
 import argparse
 import json
 import subprocess
+from datetime import datetime, timedelta
 
+def parse_date_range(date_range):
+    start_date, end_date = date_range.split('_')
+    return start_date, end_date
+
+def generate_dates(start_date, end_date):
+    dates = []
+    current_date = datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = datetime.strptime(end_date, '%Y-%m-%d')
+    while current_date <= end_date:
+        dates.append(current_date.strftime('%Y-%m-%d'))
+        current_date += timedelta(days=1)
+    return dates
 
 def main():
     parser = argparse.ArgumentParser(description='Run a container repeatedly with the given list of AGGREGATION_DATE env vars.')
@@ -32,11 +49,20 @@ def main():
     container = data['container']
     base_env = data['base_env']
     dates = data['dates']
+    date_ranges = data.get('date_ranges', [])
 
     for date in dates:
         print(f'Running container {container} with AGGREGATION_DATE={date}')
         if not args.dry_run:
             subprocess.run(['docker', 'run', f'--env-file={base_env}', '-e', f'AGGREGATION_DATE={date}', container], check=True)
+
+    for date_range in date_ranges:
+        start_date, end_date = parse_date_range(date_range)
+        dates = generate_dates(start_date, end_date)
+        for date in dates:
+            print(f'Running container {container} with AGGREGATION_DATE={date}')
+            if not args.dry_run:
+                subprocess.run(['docker', 'run', f'--env-file={base_env}', '-e', f'AGGREGATION_DATE={date}', container], check=True)
 
 if __name__ == '__main__':
     main()
