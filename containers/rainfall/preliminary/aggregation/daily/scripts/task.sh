fe799f0edf27c98dedc630b1148328a7eeff79ee
Michael Dodge <mdodge@hawaii.edu>
2023-09-05 10:03:08 -1000

    move daily dependency pull to the build

--- containers/rainfall/preliminary/aggregation/daily/scripts/task.sh
+++ containers/rainfall/preliminary/aggregation/daily/scripts/task.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "[task.sh] [1/8] Starting Execution."
+echo "[task.sh] [1/7] Starting Execution."
 export TZ="HST"
 echo "It is currently $(date)."
 if [ $AGGREGATION_DATE ]; then
@@ -13,18 +13,12 @@ fi
 export AGGREGATION_DATE_YESTERDAY=$(date --date="$AGGREGATION_DATE - 1 day" --iso-8601)
 echo "Yesterday is: " $AGGREGATION_DATE_YESTERDAY
 
-echo "[task.sh] [2/8] Acquiring Daily Dependencies."
-cd /home/hawaii_climate_products_container/preliminary/rainfall/dependencies
-wget https://ikeauth.its.hawaii.edu/files/v2/download/public/system/ikewai-annotated-data/HCDP/workflow_data/preliminary/rainfall/dependencies/daily_dependencies.tar.gz
-tar -xf daily_dependencies.tar.gz
-rm daily_dependencies.tar.gz
-
-echo "[task.sh] [3/8] Acquiring yesterday's cumulative aggregation file, if it exists."
+echo "[task.sh] [2/7] Acquiring yesterday's cumulative aggregation file, if it exists."
 cd /home/hawaii_climate_products_container
 echo "---rf_daily_wget.sh---"
 bash rf_daily_wget.sh
 
-echo "[task.sh] [4/8] Aggregating Rainfall data on the daily timeframe."
+echo "[task.sh] [3/7] Aggregating Rainfall data on the daily timeframe."
 cd /home/hawaii_climate_products_container/preliminary/rainfall/code/daily
 
 echo "---begin hads_daily_rf_FINAL.R---"
@@ -49,22 +43,22 @@ echo "---begin all_data_daily_last_obs_FINAL.R---"
 Rscript /home/hawaii_climate_products_container/preliminary/rainfall/code/daily/rcode/all_data_daily_last_obs_FINAL.R $AGGREGATION_DATE
 echo "---end all_data_daily_last_obs_FINAL.R---"
 
-echo "[task.sh] [5/8] Preparing for intermediate data upload."
+echo "[task.sh] [4/7] Preparing for intermediate data upload."
 cd /sync
 python3 update_date_string_in_config.py intermediate_upload_config.json intermediate_upload_config_datestrings_loaded.json $AGGREGATION_DATE_YESTERDAY
 python3 add_upload_list_to_config.py intermediate_upload_config_datestrings_loaded.json config.json
 python3 add_auth_info_to_config.py config.json
 
-echo "[task.sh] [6/8] Attempting to upload the aggregated intermediate data."
+echo "[task.sh] [5/7] Attempting to upload the aggregated intermediate data."
 python3 upload.py
 
-echo "[task.sh] [7/8] Preparing for production data upload."
+echo "[task.sh] [6/7] Preparing for production data upload."
 cd /sync
 python3 update_date_string_in_config.py upload_config.json upload_config_datestrings_loaded.json $AGGREGATION_DATE_YESTERDAY
 python3 add_upload_list_to_config.py upload_config_datestrings_loaded.json config.json
 python3 add_auth_info_to_config.py config.json
 
-echo "[task.sh] [8/8] Attempting to upload the aggregated production data."
+echo "[task.sh] [7/7] Attempting to upload the aggregated production data."
 python3 upload.py
 
 echo "[task.sh] All done!"
