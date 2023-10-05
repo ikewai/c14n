4b16d9e025b4c3c6fe804037066aace8e02adca0
mdodge2 <mdodge@hawaii.edu>
2023-10-04 14:03:00 -1000

    add mesonet aq container

---
+++ containers/shared/acquisition/mesonet-daily/scripts/task.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "[task.sh] [1/4] Starting Execution."
+
+echo "[task.sh] [2/4] Collecting Climate data from Synoptic on the daily timeframe."
+cd /home/hawaii_climate_products_container/preliminary/data_aqs/code/hi_mesonet
+Rscript hiMeso_24hr_data_aqs.R
+Rscript hiMeso_synoptic_24hr_data_aqs.R
+
+echo "[task.sh] [3/4] Injecting authentication variables for uploader."
+cd /sync
+cp upload_config.json config.json
+python3 add_auth_info_to_config.py config.json
+
+echo "[task.sh] [4/4] Attempting to upload the gathered data."
+python3 upload.py
+
+echo "[task.sh] All done!"
