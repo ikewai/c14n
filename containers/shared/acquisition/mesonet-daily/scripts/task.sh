7469bcadfb117216d31cc6b2bfb29044c03f1480
Michael Dodge <mdodge@hawaii.edu>
2023-10-31 09:23:58 -1000

    add debug echoes

--- containers/shared/acquisition/mesonet-daily/scripts/task.sh
+++ containers/shared/acquisition/mesonet-daily/scripts/task.sh
@@ -3,7 +3,9 @@ echo "[task.sh] [1/4] Starting Execution."
 
 echo "[task.sh] [2/4] Collecting Climate data from Synoptic on the daily timeframe."
 cd /home/hawaii_climate_products_container/preliminary/data_aqs/code/hi_mesonet
+echo "---hiMeso_24hr_data_aqs.R---"
 Rscript hiMeso_24hr_data_aqs.R
+echo "---hiMeso_synoptic_24hr_data_aqs.R---"
 Rscript hiMeso_synoptic_24hr_data_aqs.R
 
 echo "[task.sh] [3/4] Injecting authentication variables for uploader."
