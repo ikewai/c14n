17c2ba3cc49975ba3f7e45a3ec4669e1326e4288
Michael Dodge <mdodge@hawaii.edu>
2023-12-20 11:13:22 -1000

    change order of script runs

--- containers/airtemp/preliminary/aggregation/daily/scripts/task.sh
+++ containers/airtemp/preliminary/aggregation/daily/scripts/task.sh
@@ -17,6 +17,9 @@ echo "[task.sh] [2/5] Aggregating Airtemp data on the daily timeframe."
 cd /home/hawaii_climate_products_container/preliminary/air_temp/daily/code
 echo "---temp_agg_wget.py---"
 python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/temp_agg_wget.py $AGGREGATION_DATE
+echo "---temp_map_wget.py---"
+python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/temp_map_wget.py $AGGREGATION_DATE
+cp -rf dependencies/* /home/hawaii_climate_products_container/preliminary/air_temp/daily/dependencies
 echo "---hads_temp_parse.py---"
 python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/hads_temp_parse.py $AGGREGATION_DATE
 echo "---madis_temp_parse.py---"
@@ -26,9 +29,6 @@ python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/d
 
 echo "[task.sh] [3/5] Mapping Airtemp data on the daily timeframe."
 cd /home/hawaii_climate_products_container/
-echo "---temp_map_wget.py---"
-python3 -W ignore /home/hawaii_climate_products_container/preliminary/air_temp/daily/code/temp_map_wget.py $AGGREGATION_DATE
-cp -rf dependencies/* /home/hawaii_climate_products_container/preliminary/air_temp/daily/dependencies
 
 cd /home/hawaii_climate_products_container/preliminary/air_temp/daily/
 echo "---update_predictor_table.py---"
